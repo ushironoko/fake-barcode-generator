251d752f4028bac54906af06dcc1d7b98e71a59b
ushironoko <apple19940820@gmail.com>
2020-09-17 01:19:46 +0900

    fix

--- src/generator.ts
+++ src/generator.ts
@@ -1,19 +1,20 @@
-function main(len: string[]) {
-  try {
-    const r = Math.random()
-      .toString(10)
-      .slice(-len[2] + 1)
+export function barcodeGenerator(len: string) {
+  const r = Math.random()
+    .toString(10)
+    .slice(-len + 1)
 
-    const body = [...r].map((s) => +s)
+  const body = [...r].map((s) => +s)
 
-    const evenSum = body.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b)
-    const oddSum = body.filter((_, i) => i % 2 !== 0).reduce((a, b) => a + b)
+  const evenSum = body.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b)
+  const oddSum = body.filter((_, i) => i % 2 !== 0).reduce((a, b) => a + b)
 
-    const checkDesit = `${10 - +(evenSum * 3 + oddSum)}`.slice(-1)
-    console.log(`${r}${checkDesit}`)
-  } catch (e) {
-    console.error(e)
-  }
+  const checkDesit = `${10 - +(evenSum * 3 + oddSum)}`.slice(-1)
+  return `${r}${checkDesit}`
 }
 
-main(process.argv)
+function main(len: string) {
+  const result = barcodeGenerator(len)
+  console.log(result)
+}
+
+main(process.argv[2])
