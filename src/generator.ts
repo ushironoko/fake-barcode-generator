50e448262bb61dfd19b8a47d38c492b83ce9d3f8
ushironoko <apple19940820@gmail.com>
2020-09-16 18:07:12 +0900

    fix

--- src/generator.ts
+++ src/generator.ts
@@ -3,11 +3,11 @@ function main(len: string[]) {
     .toString(9)
     .slice(Number(-len[2]) + 1)
   //@ts-ignore
-  const barcodeBodyArray = [...r]
-  const evenSum = barcodeBodyArray
+  const body = [...r]
+  const evenSum = body
     .filter((_, i) => i % 2 === 0)
     .reduce((a, b) => Number(a) + Number(b))
-  const oddSum = barcodeBodyArray
+  const oddSum = body
     .filter((_, i) => i % 2 !== 0)
     .reduce((a, b) => Number(a) + Number(b))
 
