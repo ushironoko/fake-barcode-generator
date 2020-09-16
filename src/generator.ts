9e976505753a98e44b4b04ab78dcb42d663a4ea3
ushironoko <apple19940820@gmail.com>
2020-09-17 00:38:41 +0900

    fix radix

--- src/generator.ts
+++ src/generator.ts
@@ -1,7 +1,8 @@
 function main(len: string[]) {
   try {
+    console.log(Math.random().toString(10))
     const r = Math.random()
-      .toString(9)
+      .toString(10)
       .slice(-len[2] + 1)
 
     const body = [...r].map((s) => +s)
