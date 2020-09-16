49a7d0695c17731d40228718795380e4cc38c232
ushironoko <apple19940820@gmail.com>
2020-09-16 18:42:55 +0900

    refactor

--- src/generator.ts
+++ src/generator.ts
@@ -1,18 +1,19 @@
 function main(len: string[]) {
-  const r = Math.random()
-    .toString(9)
-    .slice(Number(-len[2]) + 1)
-  //@ts-ignore
-  const body = [...r]
-  const evenSum = body
-    .filter((_, i) => i % 2 === 0)
-    .reduce((a, b) => Number(a) + Number(b))
-  const oddSum = body
-    .filter((_, i) => i % 2 !== 0)
-    .reduce((a, b) => Number(a) + Number(b))
+  try {
+    const r = Math.random()
+      .toString(9)
+      .slice(-len[2] + 1)
 
-  const checkDesit = 10 - Number((evenSum * 3 + oddSum).toString().slice(-1))
-  console.log(`${r}${checkDesit}`)
+    const body = [...r].map((s) => +s)
+
+    const evenSum = body.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b)
+    const oddSum = body.filter((_, i) => i % 2 !== 0).reduce((a, b) => a + b)
+
+    const checkDesit = 10 - +(evenSum * 3 + oddSum).toString().slice(-1)
+    console.log(`${r}${checkDesit}`)
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 main(process.argv)
