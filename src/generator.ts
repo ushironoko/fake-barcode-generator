2cfe25129350ce15aba08ba94299868748302124
ushironoko <apple19940820@gmail.com>
2020-09-18 11:19:59 +0900

    処理を戻した

--- src/generator.ts
+++ src/generator.ts
@@ -1,20 +1,19 @@
-export function barcodeGenerator(len: string) {
-  const r = Math.random()
-    .toString(10)
-    .slice(-len + 1)
+function main(len: string[]) {
+  try {
+    const r = Math.random()
+      .toString(10)
+      .slice(-len[2] + 1)
 
-  const body = [...r].map((s) => +s)
+    const body = [...r].map((s) => +s)
 
-  const evenSum = body.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b)
-  const oddSum = body.filter((_, i) => i % 2 !== 0).reduce((a, b) => a + b)
+    const evenSum = body.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b)
+    const oddSum = body.filter((_, i) => i % 2 !== 0).reduce((a, b) => a + b)
 
-  const checkDesit = `${10 - (evenSum * 3 + oddSum)}`.slice(-1)
-  return `${r}${checkDesit}`
+    const checkDesit = 10 - +(evenSum * 3 + oddSum).toString().slice(-1)
+    console.log(`${r}${checkDesit}`)
+  } catch (e) {
+    console.error(e)
+  }
 }
 
-function main(len: string) {
-  const result = barcodeGenerator(len)
-  console.log(result)
-}
-
-main(process.argv[2])
+main(process.argv)
