3cdb9bd09f47ac5bbee49a9b3f1304860c28a032
ushironoko <apple19940820@gmail.com>
2020-10-15 19:12:57 +0900

    指定回数出力するように修正&チェックデジット が10の時の対応

--- src/generator.ts
+++ src/generator.ts
@@ -1,18 +1,22 @@
-function main(len: string[]) {
-  try {
-    const r = Math.random()
-      .toString(10)
-      .slice(-len[2] + 1)
+function main(argv: string[]) {
+  const l = +argv[3] || 1
+  for (let i = 0; i < l; i++) {
+    try {
+      const r = Math.random()
+        .toString(10)
+        .slice(-argv[2] + 1)
 
-    const body = [...r].map((s) => +s)
+      const body = [...r].map((s) => +s)
 
-    const evenSum = body.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b)
-    const oddSum = body.filter((_, i) => i % 2 !== 0).reduce((a, b) => a + b)
+      const evenSum = body.filter((_, i) => i % 2 === 0).reduce((a, b) => a + b)
+      const oddSum = body.filter((_, i) => i % 2 !== 0).reduce((a, b) => a + b)
 
-    const checkDesit = 10 - +(evenSum * 3 + oddSum).toString().slice(-1)
-    console.log(`${r}${checkDesit}`)
-  } catch (e) {
-    console.error(e)
+      const res = 10 - +(evenSum * 3 + oddSum).toString().slice(-1)
+      const checkDesit = res === 10 ? 1 : res
+      console.log(`${r}${checkDesit}`)
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
 
